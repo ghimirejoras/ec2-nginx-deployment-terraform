6216af8783bc55079bbd8b364b65a627b1e2d71a
ghimirejoras <ghimirejoras@gmail.com>
2025-03-30 21:38:05 +1100

    3003025: github action1

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -18,7 +18,7 @@ variable "aws_secret_access_key" {
 
 
 variable "ami_id" {
-  default = "ami-084568db4383264d"
+  default = "ami-084568db4383264d4"
 }
 variable "instance_type" {
   default = "t2.micro"
