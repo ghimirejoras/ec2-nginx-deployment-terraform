4ff1f33796ad5c54530ad0df8e82ce84d0825afa
ghimirejoras <ghimirejoras@gmail.com>
2025-04-02 22:59:34 +1100

    0204025: tfstate to s3 and state lock to use lock file

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -13,6 +13,8 @@ terraform {
     key            = ""
     region         = ""
     encrypt        = ""
-    dynamodb_table = ""
+    // can use dynamodb_table but use_lockfile is new approach 
+    // no managing of db all done in s3 itself so new is better
+    use_lockfile = ""
   }
 }
